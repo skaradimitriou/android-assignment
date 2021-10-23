2d86cafc5e7f7c82ceeef38433219b1895fe0991
stathiskaradimitriou <stathis.karadimitriou@gmail.com>
2021-10-24 00:19:48 +0300

    Elapsed time is now updated for every second

--- app/src/main/java/com/stathis/novibetassignment/ui/MainViewModel.kt
+++ app/src/main/java/com/stathis/novibetassignment/ui/MainViewModel.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.google.gson.Gson
+import com.stathis.novibetassignment.R
 import com.stathis.novibetassignment.abstraction.AbstractAndroidViewModel
 import com.stathis.novibetassignment.callbacks.ItemClickListener
 import com.stathis.novibetassignment.callbacks.MainScreenCallback
@@ -22,7 +23,7 @@ import retrofit2.Callback
 import retrofit2.Response
 
 
-class MainViewModel(app : Application) : AbstractAndroidViewModel(app),ItemClickListener {
+class MainViewModel(app: Application) : AbstractAndroidViewModel(app), ItemClickListener {
 
     val adapter = MainScreenAdapter(this)
     val gamesAdapter = MainScreenAdapter(this)
@@ -31,19 +32,19 @@ class MainViewModel(app : Application) : AbstractAndroidViewModel(app),ItemClick
     val headlines = MutableLiveData<List<BetViewItem>>()
     val isLoading = MutableLiveData<Boolean>()
     val errorResponse = MutableLiveData<Boolean>()
-    private lateinit var callback : MainScreenCallback
+    private lateinit var callback: MainScreenCallback
 
-    fun pushLoginData(){
+    fun pushLoginData() {
         val json = JSONObject().also {
-            it.put("userName","efstathios")
-            it.put("password","karadimitriou")
+            it.put(getString(R.string.username), getString(R.string.name))
+            it.put(getString(R.string.password), getString(R.string.surname))
         }
 
         ApiClient.login(json.toString()).enqueue(object : Callback<TokenResponse> {
             override fun onResponse(call: Call<TokenResponse>, response: Response<TokenResponse>) {
                 val result = response.body()
                 val code = response.code()
-                when(code == 200){
+                when (code == 200) {
                     true -> sessionManager.saveAuthToken(result!!.token_type)
                 }
 
@@ -56,53 +57,62 @@ class MainViewModel(app : Application) : AbstractAndroidViewModel(app),ItemClick
         })
     }
 
-    fun getHeadlines(){
-        ApiClient.getUpdatedHeadlines(token = "Bearer ${sessionManager.fetchAuthToken()}").enqueue(object : Callback<List<UpdatedHeadlinesItem>> {
-            override fun onResponse(call: Call<List<UpdatedHeadlinesItem>>, response: Response<List<UpdatedHeadlinesItem>>) {
-                val result = response.body()?.first()?.betViews
-                headlines.value = result
-                errorResponse.value = false
-            }
+    fun getHeadlines() {
+        ApiClient.getUpdatedHeadlines(token = "Bearer ${sessionManager.fetchAuthToken()}")
+            .enqueue(object : Callback<List<UpdatedHeadlinesItem>> {
+                override fun onResponse(
+                    call: Call<List<UpdatedHeadlinesItem>>,
+                    response: Response<List<UpdatedHeadlinesItem>>
+                ) {
+                    val result = response.body()?.first()?.betViews
+                    headlines.value = result
+                    errorResponse.value = false
+                }
 
-            override fun onFailure(call: Call<List<UpdatedHeadlinesItem>>, t: Throwable) {
-                errorResponse.value = true
-            }
-        })
+                override fun onFailure(call: Call<List<UpdatedHeadlinesItem>>, t: Throwable) {
+                    errorResponse.value = true
+                }
+            })
     }
 
-    fun getGames(){
-        ApiClient.getUpdatedGames(token = "Bearer ${sessionManager.fetchAuthToken()}").enqueue(object : Callback<List<UpdatedGamesItem>> {
-            override fun onResponse(call: Call<List<UpdatedGamesItem>>, response: Response<List<UpdatedGamesItem>>) {
-                val events = response.body()?.first()?.betviews?.first()?.competitions?.first()?.events
-                games.value = events
-                errorResponse.value = false
-            }
+    fun getGames() {
+        ApiClient.getUpdatedGames(token = "Bearer ${sessionManager.fetchAuthToken()}")
+            .enqueue(object : Callback<List<UpdatedGamesItem>> {
+                override fun onResponse(
+                    call: Call<List<UpdatedGamesItem>>,
+                    response: Response<List<UpdatedGamesItem>>
+                ) {
+                    val events =
+                        response.body()?.first()?.betviews?.first()?.competitions?.first()?.events
+                    games.value = events
+                    errorResponse.value = false
+                }
 
-            override fun onFailure(call: Call<List<UpdatedGamesItem>>, t: Throwable) {
-                errorResponse.value = true
-            }
-        })
+                override fun onFailure(call: Call<List<UpdatedGamesItem>>, t: Throwable) {
+                    errorResponse.value = true
+                }
+            })
     }
 
-    fun observe(owner : LifecycleOwner, callback : MainScreenCallback){
+    fun observe(owner: LifecycleOwner, callback: MainScreenCallback) {
         this.callback = callback
 
-        games.observe(owner,Observer{
+        games.observe(owner, Observer {
             gamesAdapter.submitList(it)
         })
 
-        headlines.observe(owner,Observer{
+        headlines.observe(owner, Observer {
             adapter.submitList(it)
         })
     }
 
-    fun release(owner: LifecycleOwner){
+    fun release(owner: LifecycleOwner) {
         games.removeObservers(owner)
         headlines.removeObservers(owner)
     }
 
     override fun onItemTap(view: View) {
-        when(view.tag){
+        when (view.tag) {
             is BetViewItem -> callback.onHeadlineTap(view.tag as BetViewItem)
             is EventsItem -> callback.onGameTap(view.tag as EventsItem)
         }
--- app/src/main/java/com/stathis/novibetassignment/ui/holders/GamesViewHolder.kt
+++ app/src/main/java/com/stathis/novibetassignment/ui/holders/GamesViewHolder.kt
@@ -1,6 +1,7 @@
 package com.stathis.novibetassignment.ui.holders
 
 import android.os.Build
+import android.os.Handler
 import android.view.View
 import androidx.annotation.RequiresApi
 import com.stathis.novibetassignment.R
@@ -9,10 +10,17 @@ import com.stathis.novibetassignment.abstraction.LocalModel
 import com.stathis.novibetassignment.callbacks.ItemClickListener
 import com.stathis.novibetassignment.models.EventsItem
 import kotlinx.android.synthetic.main.hodler_games_item.view.*
-import java.time.LocalDateTime
 import java.time.LocalTime
+import java.util.*
 
-class GamesViewHolder(itemView: View, callback: ItemClickListener) : AbstractViewHolder(itemView, callback) {
+class GamesViewHolder(itemView: View, callback: ItemClickListener) :
+    AbstractViewHolder(itemView, callback) {
+
+    var handler = Handler()
+    var runnable: Runnable? = null
+    var delay = 1000L
+    var counter = 0
+    private lateinit var calendar: Calendar
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun present(data: LocalModel) {
@@ -23,15 +31,44 @@ class GamesViewHolder(itemView: View, callback: ItemClickListener) : AbstractVie
 
                 when (data.liveData.isInPlay) {
                     true -> {
-                        val time = data.liveData.elapsed
-                        val cleanTime = LocalTime.parse(time)
+                        var cleanTime = LocalTime.parse(data.liveData.elapsed)
+
+                        calendar = Calendar.getInstance().also {
+                            it.set(Calendar.HOUR, cleanTime.hour)
+                            it.set(Calendar.MINUTE, cleanTime.minute)
+                            it.set(Calendar.SECOND, cleanTime.second)
+                        }
+
                         itemView.game_elapsed_time.text = "${cleanTime.minute}:${cleanTime.second}"
-                    } //negative time
-                    false -> {
-                        itemView.game_elapsed_time.text = itemView.resources.getString(R.string.game_ended)
                     }
                 }
+
+
+                handler.postDelayed(Runnable {
+                    handler.postDelayed(runnable!!, delay)
+
+                    counter++
+
+                    when (data.liveData.isInPlay) {
+                        true -> {
+                            calendar.add(Calendar.SECOND, 1)
+
+                            var minutes = calendar.time.minutes
+                            var second = calendar.time.seconds
+
+                            val formattedSeconds = String.format("%02d", second)
+
+                            itemView.game_elapsed_time.text = "$minutes:$formattedSeconds"
+                        }
+                        false -> {
+                            itemView.game_elapsed_time.text =
+                                itemView.resources.getString(R.string.game_ended)
+                        }
+                    }
+
+                }.also { runnable = it }, delay)
             }
         }
     }
+
 }
